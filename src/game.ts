07da2264256cbee19b858df55f7040d55ae60373
Firre De Clippel <firre.declippel@ugent.be>
2025-10-12 22:42:22 +0200

    finished setup, moved game logic to game.ts and Card to types.ts

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,6 @@
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
-const ctx = canvas.getContext('2d')
-
-
-interface Card {
-    number: number
-    suit: string
-}
+import type { Card } from "./types"
 
 const suits = ['hearts', 'diamonds', 'clubs', 'spades']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 let deck: Card[] = []
 let playerHand: Card[] = []
-
