b01a62022f56a1202fe39d6cdaabd80b2d8d9dea
fdclippe <firre.declippel@ugent.be>
2025-10-13 22:51:55 +0200

    add shuffleDeck function

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,35 @@
 import type { Card } from "./types"
 
-const suits = ['hearts', 'diamonds', 'clubs', 'spades']
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+const suits: string[] = ['hearts', 'diamonds', 'clubs', 'spades']
 let deck: Card[] = []
 let playerHand: Card[] = []
+let dealerHand: Card[] = []
+//populate deck
+for (const suit of suits) {
+    for (let j = 1; j < 14; j++) {
+        const card: Card = { number: j, suit: suit }
+        deck.push(card)
+    }
+}
+shuffleDeck(deck);
+playerHand = [deck.pop() as Card, deck.pop() as Card]
+console.log(playerHand);
+
+function shuffleDeck(array: Card[]) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        if (
+            array[randomIndex] !== undefined &&
+            array[currentIndex] !== undefined
+            
+        ) {
+            const tmp: Card = array[randomIndex];
+            array[randomIndex] = array[currentIndex] as Card;
+            array[currentIndex] = tmp;
+        }
+    }
+}
+
